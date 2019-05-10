37b78025026c0f92892a51a4f643a4195f27d032
Ranald Clouston <ranald.clouston@anu.edu.au>
2019-05-10 15:10:45 +1000

    Comitting assignment 3 for semester 1 2019

--- src/AI.hs
+++ src/AI.hs
@@ -1,27 +1,27 @@
-{-|
-Module      : AI
-Description : The AI for Othello
-Copyright   : (c) Robert 'Probie' Offner, <your name here>, 2018
-License     : GPL-3
--}
 module AI where
 
-import Game
+import SushiGo
 
--- |  An AI is a function from the time given and the current state of
--- the game to a (potentially infinite) list of increasingly better
--- moves.
-type AI = Double -> Game -> [(Int, Int)]
+-- | The type of AI functions. Do not change this.
+--
+-- The test program will repeatedly call an AI function with
+-- increasing lookahead values until it takes too long to generate a
+-- result, and use the final result it returns as the "best" move your
+-- AI could find.
+type AIFunc
+  = GameState -- ^ The current game
+  -> Int -- ^ How far you should look ahead
+  -> Move
 
--- | A list of known AIs and their names.
-ais :: [(String, AI)]
-ais = [("default", makeBestMove)]
+-- | The table of all AIs you have implemented. We will mark the AI
+-- called "default" as your submission, but you may include other AIs
+-- for testing.
+ais :: [(String, AIFunc)]
+ais = [("default", firstCard)]
 
--- | The default AI. It just repeatedly applies `makeAMove` to
--- increasingly higher depths.
-makeBestMove :: AI
-makeBestMove _timeout game = map (makeAMove game) [1..]
-
--- | Given a `Game` and a lookahead, return the best move to play
-makeAMove :: Game -> Int -> (Int, Int)
-makeAMove = error "makeAMove: Unimplemented"
+-- Equivalently: firstLegal :: GameState -> Int -> Move
+-- firstLegal simply takes the first card it sees
+firstCard :: AIFunc
+firstCard state _ = case gameStatus state of
+  Turn player -> TakeCard (head (handFor player state))
+  _ -> error "firstCard: called on finished game"
